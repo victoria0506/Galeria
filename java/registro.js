cad55af3d1b3ebc1fb702ddc3b0de8e8406300d0
victoria0506 <vicguti527@gmail.com>
2024-05-08 10:09:21 -0600

    registro listo

---
+++ java/registro.js
@@ -0,0 +1,31 @@
+
+
+const btnregistro = document.querySelector("#registro")
+
+btnregistro.addEventListener("click", (e) =>{
+    e.preventDefault()
+
+    const correo = document.querySelector("#correo").value
+    const contraseña = document.querySelector("#contraseña").value
+
+    const usuarios = JSON.parse(localStorage.getItem("usuarios")) || []
+    const valiusu = usuarios.find(usu => usu.correo === correo)
+    if (valiusu) {
+        alert("El usuario ya esta registrado")
+    }
+     usuarios.push({correo: correo, contraseña: contraseña})
+     localStorage.setItem(`usu`, JSON.stringify(usuarios))
+     alert("Registro existoso")
+     window.location.href= "login.html"
+})
+
+
+
+
+
+
+
+
+
+
+

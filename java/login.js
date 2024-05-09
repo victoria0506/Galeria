be5f3f801c905855cde90318a2f88e812a70e876
victoria0506 <vicguti527@gmail.com>
2024-05-08 22:20:17 -0600

    login-listo

--- java/login.js
+++ java/login.js
@@ -6,12 +6,13 @@ btnsesion.addEventListener("click", (e) =>{
     const contraseña = document.querySelector("#contraseña").value
     const nomUsuario = document.querySelector("#usuario").value
  
-    const usuarios = JSON.parse(localStorage.getItem("usu")) || []
+    const usuarios = JSON.parse(localStorage.getItem("usuarios")) || []
     const valiusu = usuarios.find(usu => usu.correo == correo, usu => usu.contraseña == contraseña)
     if(!valiusu){
-        alert("Datos invalidos")
+        alert("Contraseña y/o correo incorrectos")
     }else{
         alert("Bienvenido")
         window.location.href= "index.html"
+        localStorage.setItem("usuario", JSON.stringify(usuarios))
     }
 })
--- java/registro.js
+++ java/registro.js
@@ -3,20 +3,21 @@
 const btnregistro = document.querySelector("#registro")
 
 btnregistro.addEventListener("click", (e) =>{
+    
     e.preventDefault()
-
+     
     const correo = document.querySelector("#correo").value
     const contraseña = document.querySelector("#contraseña").value
 
-    const usuarios = JSON.parse(localStorage.getItem("usu")) || [] 
+    const usuarios = JSON.parse(localStorage.getItem("usuarios")) || [] 
     const valiusu = usuarios.find(usu => usu.correo === correo)
 
     if (valiusu) {
-        alert("El usuario ya esta registrado")
+        return alert("El usuario ya esta registrado")
     }
      usuarios.push({correo: correo, contraseña:contraseña})
-     localStorage.setItem("usuarios", JSON.stringify(usuarios))
      alert("Registro existoso")
+     localStorage.setItem("usuarios", JSON.stringify(usuarios))
      window.location.href= "login.html"
      
 })
